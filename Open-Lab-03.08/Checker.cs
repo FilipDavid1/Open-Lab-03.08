7fa94d94224783a88efb52e3148eda849c378442
FilipDavid1 <filipenkodavid@gmail.com>
2020-10-13 15:32:09 +0200

    Checks if a word is in the plural

--- Open-Lab-03.08/Checker.cs
+++ Open-Lab-03.08/Checker.cs
@@ -6,7 +6,9 @@ namespace Open_Lab_03._08
     {
         public bool IsPlural(string word)
         {
-            throw new NotImplementedException();
+
+
+            return word[word.Length - 1] == 's';
         }
     }
 }
